60c14e3ab52a7a8d60346275ae3b3dec6a533404
bin li <bin li>
2017-02-21 15:03:47 +0800

    Created new template and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
  
   devise_for :users
+  resources :tasks
   root 'pages#home'
 
-   get 'about' => 'pages#about'
+  get 'about' => 'pages#about'
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
