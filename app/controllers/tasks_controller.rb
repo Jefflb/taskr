d3be193913763ef8f1f2185bddcfd5462c893893
bin li <bin li>
2017-02-22 15:31:26 +0800

    Created user_task assoc and updated controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_task, only:[:show,:edit,:update,:destroy]
     def index
-        @tasks = Task.all
+        @tasks = current_user.tasks
     end
 
     def new
@@ -9,7 +10,7 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(tasks_params)
+        @task = current_user.tasks.new(tasks_params)
 
         if @task.save
          flash[:notice] = "Task was successfully created!"
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220071104) do
+ActiveRecord::Schema.define(version: 20170222070643) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "use"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
